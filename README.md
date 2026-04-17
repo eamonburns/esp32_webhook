3229c9de7daa9be24b2b70b804d802b84bab2efd
Eamon Burns <eamon.r.burns@gmail.com>
2026-04-16 21:53:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ESP32-S3 Webhook
+
+Send a webhook request from an ESP32-S3.
+
+## Prerequisites
+
+<!-- TODO: See if a specific version is required -->
+- [ESP-IDF](https://docs.espressif.com/projects/esp-idf/en/stable/esp32s3/get-started/index.html#installation) (v6.0 I think)
+
+## Resources
+
+- [Firebeetle 2 ESP32-S3 spec sheet](https://wiki.dfrobot.com/dfr0975#tech_specs)
---
+++ main/esp32_webhook.c
@@ -0,0 +1,97 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include "esp_attr.h"
+#include "freertos/idf_additions.h"
+#include "hal/gpio_types.h"
+#include "portmacro.h"
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+#include "driver/gpio.h"
+
+#define WIFI_SSID CONFIG_WIFI_SSID
+#define WIFI_PASSWORD CONFIG_WIFI_PASSWORD
+#define GPIO_INPUT_PIN CONFIG_GPIO_INPUT_PIN
+#define GPIO_INPUT_PIN_SEL (1ULL<<GPIO_INPUT_PIN)
+
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static QueueHandle_t gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void invoke_webhook() {
+    printf("Invoking webhook /s\n");
+}
+
+// static void gpio_task(void *arg) {
+//     uint32_t io_num;
+//     for (;;) {
+//         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+//             printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+//             invoke_webhook();
+//         }
+//     }
+// }
+
+void app_main(void)
+{
+    // Configure GPIO input
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    gpio_config(&io_conf);
+
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    // xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(GPIO_INPUT_PIN, gpio_isr_handler, (void*)GPIO_INPUT_PIN);
+
+    // The level that the gpio pin will change to when the button is pressed
+    int PRESS_LEVEL = 0;
+
+    uint32_t io_num;
+    int prev_level = 1;
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            int curr_level = gpio_get_level(io_num);
+            if (curr_level != prev_level) {
+                prev_level = curr_level;
+                if (curr_level == PRESS_LEVEL) invoke_webhook();
+            } else {
+                printf("Duplicate interupt level: %d\n", curr_level);
+            }
+            // printf("Count: %d\n", count);
+            // printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            // invoke_webhook();
+        }
+    }
+    // for (int count = 0; true; count++) {
+    //     printf("Count: %d\n", count);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+
+    // printf("Hello world!\n");
+    //
+    // printf("SSID: %s, Password: %s\n", WIFI_SSID, WIFI_PASSWORD);
+    //
+    // for (int i = 10; i >= 0; i--) {
+    //     printf("Restarting in %d seconds...\n", i);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+    // printf("Restarting now.\n");
+    // fflush(stdout);
+    // esp_restart();
+}
